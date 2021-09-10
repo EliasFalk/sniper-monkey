5a6cc4b2a6a135384e8fc63b04406a7285d188bf
Elias Falk <elias.falk@hotmail.com>
2021-09-10 13:57:00 +0200

    Add libgdx initial state

---
+++ desktop/src/game/sniper_monkey/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.sniper_monkey.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.sniper_monkey.SniperMonkey;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SniperMonkey(), config);
+	}
+}
