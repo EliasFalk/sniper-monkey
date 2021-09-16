1b0266f265cdd03a8d35f0a37d5ba5115dff2759
Vincent Hellner <hellner.vincent@gmail.com>
2021-09-16 20:28:08 +0200

    Add abstract GameObject class

---
+++ core/src/game/sniper_monkey/GameObject.java
@@ -0,0 +1,33 @@
+package game.sniper_monkey;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class GameObject
+{
+    private Vector2 position;
+    private Texture sprite;
+
+    public abstract void update(float deltaTime);
+
+    public void render(SpriteBatch batch)
+    {
+        batch.draw(sprite, position.x, position.y);
+    }
+
+    protected void setSprite(Texture sprite)
+    {
+        this.sprite = sprite;
+    }
+
+    protected void setPos(Vector2 position)
+    {
+        this.position = position;
+    }
+
+    public Vector2 getPos()
+    {
+        return position;
+    }
+}
