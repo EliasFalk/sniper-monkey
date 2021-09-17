77966facfe107bde23fc9a69bb33bb72cb408e2f
Elias Falk <elias.falk@hotmail.com>
2021-09-17 13:24:39 +0200

    Add player

--- core/src/game/sniper_monkey/player/Player.java
+++ core/src/game/sniper_monkey/player/Player.java
@@ -1,7 +1,41 @@
 package game.sniper_monkey.player;
 
-public class Player {
-    int cooldown;
-    //Fighter primaryFighter;
-    //Fighter secondaryFighter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import game.sniper_monkey.world.GameObject;
+
+import java.util.Vector;
+
+public class Player extends GameObject {
+
+
+
+
+    /**
+     * Creates a player with a sprite and a position in the world
+     * @param position
+     * @param sprite
+     */
+    public Player(Vector2 position, Sprite sprite) {
+        super(position, sprite);
+
+    }
+
+    /**
+     * Creates a Player object with a sprite
+     * @param sprite
+     */
+    public Player(Sprite sprite) {
+        super(sprite);
+    }
+
+    /**
+     * Updates the class player every frame
+     * @param deltaTime
+     */
+    @Override
+    public void update(float deltaTime) {
+    }
 }
