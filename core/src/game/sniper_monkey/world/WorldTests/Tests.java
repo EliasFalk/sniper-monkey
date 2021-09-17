ce2a0f91b6ef28b0ede0bcd4442e90523e431d42
Dadi Andrason <dadi.andrason@gmail.com>
2021-09-17 15:59:28 +0200

    fix tests

---
+++ core/src/game/sniper_monkey/world/WorldTests/Tests.java
@@ -0,0 +1,16 @@
+package game.sniper_monkey.world.WorldTests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Tests {
+
+    @Test
+    public void worldTest() {
+        int dab = 2 + 2;
+        assertEquals(4, dab);
+    }
+}
+
