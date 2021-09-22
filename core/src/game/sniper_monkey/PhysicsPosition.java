ff840e906cc02540e31c625b5ceec323a1d66190
Elias Falk <elias.falk@hotmail.com>
2021-09-22 14:28:38 +0200

    Add PhysicsPosition

---
+++ core/src/game/sniper_monkey/PhysicsPosition.java
@@ -0,0 +1,71 @@
+package game.sniper_monkey;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class PhysicsPosition {
+    private final float GRAVITY = -200f;
+    private float DRAG = 50f;
+    private Vector2 position;
+    private Vector2 velocity;
+    private Vector2 acceleration = new Vector2(0, GRAVITY);
+
+
+    /**
+     * Updates the velocity and position based on current velocity and acceleration.
+     * @param deltaTime The time between frames
+     */
+    public void update(float deltaTime) {
+        position.add(velocity.cpy().scl(deltaTime));
+        velocity.add(acceleration.cpy().scl(deltaTime));
+        if(Math.abs(velocity.x) - DRAG*deltaTime < 0) {
+            velocity.x = 0;
+        } else {
+            velocity.x -= Math.signum(velocity.x)*DRAG*deltaTime;
+        }
+        acceleration.x = 0;
+    }
+
+
+    public Vector2 getPosition() {
+        return position.cpy();
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
+
+    public Vector2 getVelocity() {
+        return velocity.cpy();
+    }
+
+    public void setVelocity(Vector2 velocity) {
+        this.velocity = velocity;
+    }
+
+    public Vector2 getAcceleration() {
+        return acceleration.cpy();
+    }
+
+    public void setAcceleration(Vector2 acceleration) {
+        this.acceleration = acceleration;
+    }
+
+
+    public PhysicsPosition(Vector2 position, Vector2 velocity, Vector2 acceleration) {
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+    }
+
+    public PhysicsPosition(Vector2 position, Vector2 velocity) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    public PhysicsPosition(Vector2 position) {
+        this.position = position;
+        this.velocity = new Vector2(0,0);
+    }
+
+
+}
