962c04acfc1efc830cd2f702f7a10985b49c390d
Elias Falk <elias.falk@hotmail.com>
2021-09-22 12:30:04 +0200

    Add player input actions hash

--- core/src/game/sniper_monkey/player/Player.java
+++ core/src/game/sniper_monkey/player/Player.java
@@ -1,14 +1,23 @@
 package game.sniper_monkey.player;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import game.sniper_monkey.world.GameObject;
 
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Player extends GameObject {
+
+    private final Map<PlayerInputAction, Boolean> inputActions = new HashMap<PlayerInputAction, Boolean>();
+
+    /**
+     * Updates the inputActions map by setting the action to true, which translates to "player try to do this action".
+     * @param action the input action to be set.
+     */
+    public void setInputAction(PlayerInputAction action) {
+        inputActions.replace(action, true);
+    }
 
     /**
      * Creates a player with a sprite and a position in the world
@@ -17,7 +26,6 @@ public class Player extends GameObject {
      */
     public Player(Vector2 position, Sprite sprite) {
         super(position, sprite);
-
     }
 
     /**
---
+++ core/src/game/sniper_monkey/player/PlayerInputAction.java
@@ -0,0 +1,10 @@
+package game.sniper_monkey.player;
+
+public enum PlayerInputAction {
+    JUMP,
+    MOVELEFT,
+    MOVERIGHT,
+    ATTACK1,
+    ATTACK2,
+    BLOCK,
+}
