81a53771928a03d36352136e2dfea98fc0021efd
Kevin Jeryd <kevinjeryd01@gmail.com>
2021-09-25 17:29:35 +0200

    Add timer

---
+++ core/src/game/sniper_monkey/world/Timer.java
@@ -0,0 +1,44 @@
+package game.sniper_monkey.world;
+
+public class Timer {
+    private final long NANO = 1000000000; // Number of nanoseconds in a second
+
+    //For tracking timer start, end and duration
+    long start;
+    long end;
+    long seconds;
+
+    /**
+     * Creates the timer object
+     * @param seconds
+     */
+    public Timer(int seconds)
+    {
+        this.seconds = seconds * NANO;
+        start = end = -1;
+    }
+
+    /**
+     * Activates the timer
+     */
+    public void start()
+    {
+        System.out.println("Hello");
+        start = System.nanoTime();
+        end  = start + seconds;
+    }
+
+    /**
+     * Checks if the timer is done and deactivates it, if it is.
+     */
+    public boolean isDone()
+    {
+        if (start > 0 && end > 0)
+            if (System.nanoTime() >= end) {
+                start = end = -1;
+                return true;
+            }
+
+        return false;
+    }
+}
