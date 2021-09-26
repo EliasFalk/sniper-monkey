1ededd668241f3a2f17c1c860b7c67109d16a3f2
EliasFalk <elias.falk@hotmail.com>
2021-09-26 16:21:42 +0200

    Add fighter classes

---
+++ core/src/game/sniper_monkey/player/fighter/EvilWizard.java
@@ -0,0 +1,16 @@
+package game.sniper_monkey.player.fighter;
+
+import game.sniper_monkey.player.fighter.attack.IAttack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EvilWizard extends Fighter {
+
+    public EvilWizard() {
+        super(1, 1, 1);
+
+        // attacks.add(SomeAttack);
+    }
+
+}
--- core/src/game/sniper_monkey/player/fighter/Fighter.java
+++ core/src/game/sniper_monkey/player/fighter/Fighter.java
@@ -1,13 +1,56 @@
 package game.sniper_monkey.player.fighter;
 
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
-import game.sniper_monkey.world.GameObject;
+import game.sniper_monkey.player.fighter.attack.IAttack;
 
-public class Fighter  {
-    Sprite sprite;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Fighter(Sprite sprite) {
-        this.sprite = sprite;
+public abstract class Fighter {
+
+    public final float ATTACK_FACTOR;
+    public final float DEFENSE_FACTOR;
+    public final float SPEED_FACTOR;
+    protected final List<IAttack> attacks = new ArrayList<>();
+
+    public Fighter(float attackFactor, float defenseFactor, float speedFactor) {
+        this.ATTACK_FACTOR = attackFactor;
+        this.DEFENSE_FACTOR = defenseFactor;
+        this.SPEED_FACTOR = speedFactor;
     }
+
+    /**
+     * Returns the number of attacks the fighter has.
+     *
+     * @return An int between 0..n representing the number of attacks the fighter has.
+     */
+    public int getTotalAttacks() {
+        return attacks.size();
+    }
+
+    /**
+     * Performs the attack specified by the attackNum.
+     *
+     * @param attackNum A number between 0..n which determines which of the (n-1) attacks to perform.
+     */
+    public void performAttack(int attackNum) {
+        if (attackNum >= attacks.size()) {
+            // TODO throw error?
+        }
+        attacks.get(attackNum).performAttack(ATTACK_FACTOR); // attackModifier for when blocked for a while? -> attackDmg need to decrease
+    }
+
+    /**
+     * Returns an int between 0..100 representing the cost of performing the attack specified by attackNum.
+     *
+     * @param attackNum An int 0..n representing a attack of the fighter.
+     * @return The stamina cost of the attack.
+     */
+    public int getStaminaDecrease(int attackNum) {
+        if (attackNum >= attacks.size()) {
+            // TODO throw error?
+        }
+        return attacks.get(attackNum).getStaminaCost();
+    }
+
+
 }
