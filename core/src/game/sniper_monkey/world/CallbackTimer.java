3f23089e3d8c81f9d93167ed8c8401617aba3aa3
EliasFalk <elias.falk@hotmail.com>
2021-09-29 10:15:02 +0200

    Add CallbackTimer documentation and small additions to the class.

--- core/src/game/sniper_monkey/world/CallbackTimer.java
+++ core/src/game/sniper_monkey/world/CallbackTimer.java
@@ -11,6 +11,14 @@ public class CallbackTimer {
     private final Callback callback;
     private boolean looping;
 
+    /**
+     * Creates a timer with a callback method which will be called upon when the timer finishes.
+     * The timer does not start automatically, need to call start() for the timer to start.
+     *
+     * @param timerLength The length of the timer in seconds.
+     * @param loop        Set true if timer should restart each time it finishes.
+     * @param callback    The method that will be called each time the timer finishes.
+     */
     public CallbackTimer(float timerLength, boolean loop, Callback callback) {
         this.callback = callback;
         this.timerLength = timerLength;
@@ -18,20 +26,40 @@ public class CallbackTimer {
         this.looping = loop;
     }
 
+    /**
+     * Creates a timer with a callback method which will be called upon when the timer finishes.
+     * The timer does not start automatically, need to call start() for the timer to start.
+     *
+     * @param timerLength The length of the timer in seconds.
+     * @param callback    The method that will be called each time the timer finishes.
+     */
     public CallbackTimer(float timerLength, Callback callback) {
         this(timerLength, false, callback);
     }
 
+    /**
+     * Sets the running state of the timer to true.
+     * Does not reset the timer, will only make the timer update with deltaTime on update.
+     */
     public void start() {
         running = true;
     }
 
+    /**
+     * Updates the timer by decreasing the timeLeft by deltaTime.
+     * The timer finishes when the time left <= 0.
+     * If the timer is done it will call the callback method set when creating the timer and set the time left to 0 and stop running.
+     * If the timer is set to looping it will reset and start the timer and call the callback method each time the timer finishes.
+     *
+     * @param deltaTime The time between frames.
+     */
     public void update(float deltaTime) {
-        if(!running) return;
+        if (!running) return;
         timeLeft -= deltaTime;
         if (timeLeft <= 0) {
             running = false;
-            if(looping) {
+            timeLeft = 0;
+            if (looping) {
                 reset();
                 start();
             }
@@ -39,20 +67,58 @@ public class CallbackTimer {
         }
     }
 
+    /**
+     * Resets the time left to the initial starting time.
+     * Stops the timer.
+     */
     public void reset() {
         timeLeft = timerLength;
         running = false;
     }
 
+    /**
+     * Stops the timer.
+     */
     public void stop() {
         running = false;
     }
 
+    /**
+     * Set if the timer will restart each time it finishes.
+     *
+     * @param loop True will set timer to restart each time it finishes, false will make timer stop running on finish.
+     */
     public void setLooping(boolean loop) {
         this.looping = loop;
     }
 
+    /**
+     * The time left of the timer in seconds, returns 0 if the timer has finished.
+     *
+     * @return The time left of the timer in seconds.
+     */
     public float getTimeLeft() {
         return timeLeft;
+    }
+
+    /**
+     * Returns the state of the timer, if it's finished or not.
+     * If looping this will always return false.
+     * If running it will return false.
+     * Returns true only if the timer is not running, no time left and is not looping.
+     *
+     * @return The done-state of the timer.
+     */
+    public boolean isDone() {
+        return !running && timeLeft == 0 && !looping;
+    }
+
+    /**
+     * Returns the running-state of the timer.
+     *
+     * @return True if the timer is running, false if not running.
+     */
+    public boolean isRunning() {
+        return running;
     }
 }
