24fa01dd29d41f8d201e64dc43e8c2502eff15eb
Kevin Jeryd <kevinjeryd01@gmail.com>
2021-10-06 11:47:47 +0200

    Add read time from config in WorldTest.java

--- core/src/test/sniper_monkey/world/WorldTest.java
+++ core/src/test/sniper_monkey/world/WorldTest.java
@@ -1,20 +1,36 @@
 package sniper_monkey.world;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.math.Vector2;
 import game.sniper_monkey.model.Config;
 import game.sniper_monkey.model.player.PlayerFactory;
 import game.sniper_monkey.model.world.GameObject;
 import game.sniper_monkey.model.world.World;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WorldTest {
 
     World world = World.getInstance();
+    static String cfg;
+    static float roundTime;
+
+    @BeforeClass
+    public static void init() {
+        cfg = "cfg/game.cfg";
+        final HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        new HeadlessApplication(new ApplicationAdapter() {
+        }, config);
+        Config.readConfigFile(cfg);
+        roundTime = Config.getNumber(cfg, "ROUND_TIME");
+    }
 
     @Test
     public void testGetRoundDuration() {
-        Assert.assertEquals(130, world.getRoundDuration());
+        Assert.assertEquals(roundTime, world.getRoundDuration(), 0);
     }
 
     @Test
@@ -28,6 +44,6 @@ public class WorldTest {
     @Test
     public void testUpdate() {
         world.update(2);
-        Assert.assertEquals(127, world.getRoundDuration());
+        Assert.assertEquals(roundTime-3, world.getRoundDuration(), 0);
     }
 }
