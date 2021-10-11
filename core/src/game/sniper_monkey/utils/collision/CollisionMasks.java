4966dc9d53ba620722188efe785d9ebecce018ef
Vincent Hellner <hellner.vincent@gmail.com>
2021-10-11 16:06:56 +0200

    Add documentation

--- core/src/game/sniper_monkey/utils/collision/CollisionMasks.java
+++ core/src/game/sniper_monkey/utils/collision/CollisionMasks.java
@@ -14,6 +14,12 @@ public final class CollisionMasks
     public static final int WALL = 0b0100;
     public static final int PROJECTILE = 0b1000;
 
+    /**
+     * Checks if two collision masks are overlapping, i.e they mask each other out and should not collide
+     * @param mask1 The first mask
+     * @param mask2 The second mask
+     * @return      A boolean representing whether or not the overlap
+     */
     public static boolean areMasksOverlapping(int mask1, int mask2) {
         return (mask1 & mask2) > 0;
     }
