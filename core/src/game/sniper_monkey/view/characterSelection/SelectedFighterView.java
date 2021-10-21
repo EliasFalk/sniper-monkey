f4787a5da306be424e35822dcd06b9ed11d0c228
Kevin Jeryd <kevinjeryd01@gmail.com>
2021-10-21 13:07:21 +0200

    Add preparation for SelectedFighterView

---
+++ core/src/game/sniper_monkey/view/characterSelection/SelectedFighterView.java
@@ -0,0 +1,30 @@
+package game.sniper_monkey.view.characterSelection;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import game.sniper_monkey.model.player.fighter.Fighter;
+
+public class SelectedFighterView {
+
+    private Stage stage;
+
+    public SelectedFighterView(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void drawPlayer1PrimaryFighter(Fighter fighter) {
+
+    }
+
+    public void drawPlayer1SecondaryFighter(Fighter fighter) {
+
+    }
+
+    public void drawPlayer2PrimaryFighter(Fighter fighter) {
+
+    }
+
+    public void drawPlayer2SecondaryFighter(Fighter fighter) {
+
+    }
+
+}
